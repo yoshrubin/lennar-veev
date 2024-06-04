bdc54860dadef4b0fadc2ba350556fd6d3bd71a1
Yoshi Rubin <yoshrubin@gmail.com>
2024-06-04 12:05:28 +0300

    remove unused

---
+++ src/views/UserManagementView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import UserManagement from '@/components/UserManagement.vue'
+import { userData } from '@/user-data'
+</script>
+
+<template>
+  <main>
+    <UserManagement :userData />
+  </main>
+</template>
