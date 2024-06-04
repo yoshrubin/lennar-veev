31fb6dbcd46d480a11e42ff33dbf52ef72586581
Yoshi Rubin <yoshrubin@gmail.com>
2024-06-04 11:02:23 +0300

    init and start on app layout

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import { userData } from './user-data'
+import TheHeader from './components/TheHeader.vue'
+import UserManagement from './components/UserManagement.vue'
+import TheFooter from './components/TheFooter.vue'
+
+const currentUser = userData[0] //would probably be in state management in large scale app to avoid prop drilling
+</script>
+
+<template>
+  <TheHeader :user="currentUser" />
+  <UserManagement />
+  <TheFooter />
+  <RouterView />
+</template>
